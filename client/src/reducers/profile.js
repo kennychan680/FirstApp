4d3c92ba2f3b7e92ad2d532d68a28690335dc64f
Chan <kenny.chan@cgi.com>
2020-08-22 19:39:31 -0400

    test-22

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -45,6 +45,7 @@ export default function (state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
+        loading: false,
       };
     case GET_REPOS:
       return {
